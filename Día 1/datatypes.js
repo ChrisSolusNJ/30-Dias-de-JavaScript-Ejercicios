803c4bb4f05b3a11ad5134bdf742671c2e155231
SolusDev <chrisezprogramming@gmail.com>
2024-01-06 16:34:26 -0600

    Solución de Ejercicios Día 1

---
+++ Día 1/datatypes.js
@@ -0,0 +1,8 @@
+//Comprobar el tipo de variables con el operador typeof
+
+console.log("Type of 'Christian'", typeof 'Christian');
+console.log("Type of 5", typeof 5);
+console.log("Type of true", typeof true);
+console.log("Type of [1, 'Hola', true]", typeof [1, 'Hola', true]);
+console.log("Type of null", typeof null);
+
---
+++ Día 1/variables.js
@@ -0,0 +1,41 @@
+//Los comentarios pueden hacer que el codigo sea legible
+//Bienvenido a 30DaysOfJavaScript
+/*
+    Los comentarios pueden hacer el código legible,
+    fácil de usar e informativo
+*/
+//Declarar tipos de variables
+var cadena = "Christian";
+var b = true;
+var valor;
+var vacio = null;
+
+//Declara 4 variables sin valor
+var a;
+var c;
+var x;
+var y;
+
+//Declara 4 variables con valo
+var x1 = 'hola';
+var x2 = 314;
+var x3 = 3.1416;
+var x4 = false;
+
+//Declarar variables para almacenar nombre, apellido, estado civil y edad en mutiples lineas
+let nombre = 'Christian';
+let apellido = 'Navarro';
+let estado_civil = 'Soltero';
+let edad = 27;
+
+//Declarar variables para almacenar nombre, apellido, estado civil y edad en una sola linea
+let nombre1 = 'Ricardo', apellido1 = 'Jiménez', estadoCivil = 'Soltero', edad1 = 27;
+
+
+//Declara dos variables miEdad y tuEdad y asignales valores iniciales y registrese en la consola del navegador
+
+let miEdad = 27;
+let tuEdad = 26;
+
+console.log(`Tengo ${miEdad} años.`);
+console.log(`Tú tienes ${tuEdad} años`)
