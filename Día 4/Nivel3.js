a70d6a28c86e08db48d5e9867ebe3dacdab8a513
SolusDev <chrisezprogramming@gmail.com>
2024-01-11 00:15:19 -0600

    Teerminado de Ejercicios Nivel 3

---
+++ Día 4/Nivel3.js
@@ -0,0 +1,55 @@
+//1. Escribe un programa que diga el número de días en un mes.
+const mes = prompt('Introduce el nombre de un mes');
+if(mes === 'Enero'){
+    alert(`${mes} tiene 31 dias`);
+}else if(mes === 'Febrero'){
+    alert(`${mes} tiene 28 dias`);
+}else if(mes === 'Marzo'){
+    alert(`${mes} tiene 31 dias`);
+}else if(mes === 'Abril'){
+    alert(`${mes} tiene 30 dias`);
+}else if(mes === 'Mayo'){
+    alert(`${mes} tiene 31 dias`);
+}else if(mes === 'Junio'){
+    alert(`${mes} tiene 30 dias`);
+}else if(mes === 'Julio'){
+    alert(`${mes} tiene 31 dias`);
+}else if(mes === 'Agosto'){
+    alert(`${mes} tiene 31 dias`);
+}else if(mes === 'Septiembre'){
+    alert(`${mes} tiene 30 dias`);
+}else if(mes === 'Octubre'){
+    alert(`${mes} tiene 31 dias`);
+}else if(mes === 'Noviembre'){
+    alert(`${mes} tiene 30 dias`);
+}else if(mes === 'Diciembre'){
+    alert(`${mes} tiene 31 dias`);
+}
+
+//2. Escribe un programa que diga el número de días en un mes, ahora considera un año bisiesto.
+const mes1 = prompt('Introduce el nombre de un mes');
+if(mes1 === 'Enero'){
+    alert(`${mes1} tiene 31 dias`);
+}else if(mes1 === 'Febrero'){
+    alert(`${mes1} tiene 29 dias`);
+}else if(mes1 === 'Marzo'){
+    alert(`${mes1} tiene 31 dias`);
+}else if(mes1 === 'Abril'){
+    alert(`${mes1} tiene 30 dias`);
+}else if(mes1 === 'Mayo'){
+    alert(`${mes1} tiene 31 dias`);
+}else if(mes1 === 'Junio'){
+    alert(`${mes1} tiene 30 dias`);
+}else if(mes1 === 'Julio'){
+    alert(`${mes1} tiene 31 dias`);
+}else if(mes1 === 'Agosto'){
+    alert(`${mes1} tiene 31 dias`);
+}else if(mes1 === 'Septiembre'){
+    alert(`${mes1} tiene 30 dias`);
+}else if(mes1 === 'Octubre'){
+    alert(`${mes1} tiene 31 dias`);
+}else if(mes1 === 'Noviembre'){
+    alert(`${mes1} tiene 30 dias`);
+}else if(mes1 === 'Diciembre'){
+    alert(`${mes1} tiene 31 dias`);
+}
