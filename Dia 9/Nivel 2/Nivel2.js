059b61ca1286acf86ea8dc2b202d7e839fdc4359
SolusDev <chrisezprogramming@gmail.com>
2024-02-02 01:54:19 -0600

    Inicio e instrucciones nivel 2

---
+++ Dia 9/Nivel 2/Nivel2.js
@@ -0,0 +1,38 @@
+const countries = ["Finland", "Sweden", "Denmark", "Norway", "Iceland"];
+const names = ["Asabeneh", "Mathias", "Elias", "Brook"];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const products = [
+  { product: "banana", price: 3 },
+  { product: "mango", price: 6 },
+  { product: "potato", price: " " },
+  { product: "avocado", price: 8 },
+  { product: "coffee", price: 10 },
+  { product: "tea", price: "" },
+];
+
+// Encuentre el precio total de los productos encadenando dos o más iteradores 
+//de matrices (por ejemplo, arr.map(callback).filter(callback).reduce(callback)).
+
+
+// Encuentre la suma del precio de los productos usando sólo reduce(callback)).
+
+
+// Declara una función llamada categorizeCountries que retorna un array de países 
+//que tienen algún patrón común (encuentras el array de países en este repositorio 
+//como countries.js(ej 'land', 'ia', 'island','stan')).
+
+
+// Cree una función que retorne un array de objetos, que es la letra y el número 
+//de veces que la letra usa para empezar el nombre de un país.
+
+
+// Declara una función getFirstTenCountries y retorna un array de diez países. 
+//Utiliza diferente programación funcional para trabajar en el array countries.js.
+
+
+// Declara una función getLastTenCountries que devuelve los últimos diez países 
+//del array de países.
+
+
+// Encuentre qué letra se utiliza muchas veces como inicial de un nombre de país 
+//del array de países (ej. Finland, Fiji, France etc)
